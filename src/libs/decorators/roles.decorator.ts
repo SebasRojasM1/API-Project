ea2c388371a4908ce10369f69b682716efc72959
Sebastian <sebasrojasm1@gmail.com>
2024-05-08 18:13:07 -0500

    create the rol guardian and update the decorator

--- src/libs/decorators/roles.decorator.ts
+++ src/libs/decorators/roles.decorator.ts
@@ -1,4 +1,5 @@
 import { Role } from '../common/enums/rol.enum';
 import { SetMetadata } from '@nestjs/common';
 
-export const Roles = (role: Role) => SetMetadata('roles', role);
+export const ROLES_KEY = 'roles';
+export const Roles = (role: Role) => SetMetadata(ROLES_KEY, role);
