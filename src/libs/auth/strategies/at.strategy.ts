e83093f15e2e351ab14924d861b47952afdc6e37
Sebastian <sebasrojasm1@gmail.com>
2024-05-08 16:31:47 -0500

    Create the strategy of JWT

--- src/libs/auth/strategies/at.strategy.ts
+++ src/libs/auth/strategies/at.strategy.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
 import { JwtPayload } from '../types';
 
 @Injectable()
